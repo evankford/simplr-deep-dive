02d9b3ab10c2a5b03ca2a6c535b5d9158c52edc7
Evan Ford <evankerrickford@gmail.com>
2020-06-04 21:24:13 -0400

    Mobile wall fix animations

--- web/app/themes/deeperdive/resources/views/partials/mobilewall.blade.php
+++ web/app/themes/deeperdive/resources/views/partials/mobilewall.blade.php
@@ -4,11 +4,11 @@
 <aside  class="fixed flex w-full h-screen text-white z-101 lg:hidden bg-blue">
 
   <div class="m-auto w-85p">
-    <div data-anim-in class="w-48 mx-auto my-4 max-w-3xs">
+    <div class="w-48 mx-auto my-4 max-w-3xs">
       @include('partials.image-element', ['image'=> $mobile_logo, 'args' => ['max_width' => 600]])
     </div>
     <h2 class="header-resp">{{$mobile_title}}</h2>
-    <h3 data-anim-in class="mt-3 subheader-resp">{!!$mobile_text !!}</h3>
+    <h3 class="mt-3 subheader-resp">{!!$mobile_text !!}</h3>
   </div>
 
 </aside>
